2c9f4562c9cbdb8fa8adc8233ced8fa15fa3ec9a
Raphael Bernhard <beraphae@gmail.com>
2022-06-10 22:39:17 +0000

    Initial freertos compilation works

--- freertos_build/src/lib.rs
+++ freertos_build/src/lib.rs
@@ -8,20 +8,20 @@ mod config;
 pub mod ports;
 
 pub struct Port {
-    target_triplet: String,
-    directory: String,
-    sources: Vec<String>,
+    pub target_triplet: String,
+    pub directory: PathBuf,
+    pub sources: Vec<PathBuf>,
 }
 
-fn kernel_sources() -> Vec<String> {
+fn kernel_sources() -> Vec<PathBuf> {
     vec![
-        "croutine.c".to_string(),
-        "event_groups.c".to_string(),
-        "list.c".to_string(),
-        "queue.c".to_string(),
-        "stream_buffer.c".to_string(),
-        "tasks.c".to_string(),
-        "timers.c".to_string(),
+        "croutine.c".into(),
+        "event_groups.c".into(),
+        "list.c".into(),
+        "queue.c".into(),
+        "stream_buffer.c".into(),
+        "tasks.c".into(),
+        "timers.c".into(),
     ]
 }
 
@@ -33,14 +33,16 @@ fn get_workspace_dir() -> Result<PathBuf> {
     Ok(workspace_dir.to_owned())
 }
 
-fn get_source_paths(port: &Port) -> Result<Vec<PathBuf>> {
-    let mut all_sources = kernel_sources();
-    all_sources.extend_from_slice(&port.sources);
+struct BuildFiles {
+    sources: Vec<PathBuf>,
+    includes: Vec<PathBuf>,
+}
 
+fn get_source_paths(port: &Port) -> Result<BuildFiles> {
     let mut kernel_dir = get_workspace_dir()?;
     kernel_dir.push("FreeRTOS-Kernel");
 
-    let ret = all_sources
+    let kernel_sources: Vec<_> = kernel_sources()
         .drain(..)
         .map(|x| {
             let mut source_file = PathBuf::from(&kernel_dir);
@@ -48,17 +50,54 @@ fn get_source_paths(port: &Port) -> Result<Vec<PathBuf>> {
             source_file
         })
         .collect();
+
+    let mut port_directory = kernel_dir.clone();
+    port_directory.push("portable");
+    port_directory.push("GCC");
+    port_directory.push(port.directory.clone());
+    let port_sources: Vec<_> = port
+        .sources
+        .iter()
+        .map(|x| {
+            let mut source_file = PathBuf::from(&port_directory);
+            source_file.push(x);
+            source_file
+        })
+        .collect();
+
+    let mut sources = kernel_sources;
+    sources.extend(port_sources);
+
+    let mut kernel_include_dir = kernel_dir.clone();
+    kernel_include_dir.push("include");
+
+    let ret = BuildFiles {
+        sources,
+        includes: vec![kernel_dir, kernel_include_dir, port_directory],
+    };
     Ok(ret)
 }
 
 fn build_freertos(port: &Port, config: &Config, target_dir: &Path) -> Result<()> {
-    let sources = get_source_paths(port);
+    let sources = get_source_paths(port)?;
 
     let mut target_dir = PathBuf::from(target_dir);
     target_dir.push("freertos");
     std::fs::create_dir_all(&target_dir).unwrap();
-    target_dir.push("FreeRTOSConfig.h");
-    std::fs::write(&target_dir, config.render()?)?;
+    let mut config_file = target_dir.clone();
+    config_file.push("FreeRTOSConfig.h");
+    std::fs::write(&config_file, config.render()?)?;
+
+    let mut build = cc::Build::new();
+    println!("Sources: {:?}", sources.sources);
+    println!("Includes: {:?}", sources.includes);
+    build.files(&sources.sources);
+    build.includes(&sources.includes);
+    build.include(&target_dir);
+    build.target(&port.target_triplet);
+    build.out_dir(target_dir);
+    println!("HIA!!!!");
+    build.compile("freertos");
 
     Ok(())
 }
--- freertos_build/src/ports.rs
+++ freertos_build/src/ports.rs
@@ -2,8 +2,8 @@ use crate::Port;
 
 pub fn cortex_m4f() -> Port {
     Port {
-        target_triplet: "arm-none-eabi".to_string(),
-        sources: vec!["port.c".to_string()],
-        directory: "GCC/ARM_CM4F".to_string(),
+        target_triplet: "thumbv7em-none-eabihf".to_string(),
+        sources: vec!["port.c".into()],
+        directory: "ARM_CM4F".into(),
     }
 }
